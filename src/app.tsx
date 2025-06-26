54cb1435cd71d8279162c0e2eb832d3a1b93838a
Zoltan Kiss <zoltan.kiss@accenture.com>
2025-06-26 13:03:10 +0200

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client';
+
+const rootElement = document.getElementById('app');
+
+const root = createRoot(rootElement);
+root.render(<h2>Hello from React! xx</h2>);
